57e60db9454046176bbd350d7467ba6791235eeb
YunusEmreGunaydin <emre.gunaydin@baun.edu.tr>
2022-03-25 21:16:35 +0300

    ClassMetotDemo

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+      
+
+        public void Add(Customer customer)
+        {
+
+            Console.WriteLine(customer.FirstName +" "+ customer.LastName + " " + "added.");
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName + " " + customer.LastName + " " +"deleted.");
+        }
+        public void List(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName +" "+ "listed.");
+        }
+        public void AllProperties(Customer customer)
+        {
+            Console.WriteLine(" Id= {0} First Name= {1} Last Name= {2} Age= {3} ",customer.Id,customer.FirstName,customer.LastName,customer.Age);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            CustomerManager customerManager = new CustomerManager();
+
+            customer.Id = 1;
+            customer.FirstName = "John";
+            customer.LastName = "Wich";
+            customer.Age = 43;
+            customerManager.Add(customer);
+            customerManager.Delete(customer);
+            customerManager.List(customer);
+            customerManager.AllProperties(customer);
+
+            
+
+        }
+    }
+}
